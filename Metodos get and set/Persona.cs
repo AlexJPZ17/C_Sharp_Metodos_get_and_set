6b80089e4306275c622373ac42ac52934481b41a
AlexJPZ17 <alexjpz17@gmail.com>
2014-01-01 14:36:42 -0600

    C Sharp Metodos get and set

---
+++ Metodos get and set/Persona.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos_get_and_set
+{
+    class Persona
+    {
+        //Declaramos los atributos 
+        private string Nombre;
+        private string Apellido;
+        private int Edad;
+        /*Método constructor de la clase persona que recibe los parámetros el 
+            nombre apellido edad  */
+        public Persona(string Nombre, string Apellido, int Edad)
+        {
+            //Asignamos los parámetros a los atributos
+            this.Nombre = Nombre;
+            this.Apellido = Apellido;
+            this.Edad = Edad;
+        }
+        // Una propiedad de lectura y escritura de tipo cadena
+        public string PerNombre
+        {
+            //método get sirve para obtener los datos
+            get
+            {
+                return Nombre;
+            }
+            //Método set para asignar datos o modificar
+            set
+            {
+                Nombre = value;
+            }
+        }
+        // Una propiedad de lectura y escritura de tipo cadena
+        public string PerApellido
+        {
+            //método get sirve para obtener los datos
+            get
+            {
+                return Apellido;
+            }
+            //Método set para asignar datos o modificar
+            set
+            {
+                Apellido = value;
+            }
+        }
+        // Una propiedad de lectura  de tipo int
+        public int PerEdad
+        {
+            //método get sirve para obtener los datos
+            get
+            {
+                return Edad;
+            }
+        }
+    }
+}
---
+++ Metodos get and set/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos_get_and_set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Llamamos la clase persona y hacemos un objeto llamado per
+            Persona per = new Persona("Alex", "Pagoada", 29);
+            per.PerNombre = "Joel";
+            Console.WriteLine("Su nombre es: " + per.PerNombre);
+            Console.WriteLine("Su apellido es: " + per.PerApellido);
+            Console.WriteLine("Su edad es: " + per.PerEdad);
+            Console.ReadLine();
+        }
+    }
+}
